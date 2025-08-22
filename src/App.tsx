4b0bd2934d47b09dd717d44dc94fc4dd4150c9ad
SAHILSHANGLOO35 <sahilshangloo35@gmail.com>
2025-08-22 13:21:13 +0530

    Done with the airdropping of SOL on devnet

--- src/App.tsx
+++ src/App.tsx
@@ -10,20 +10,30 @@ import {
   WalletMultiButton,
 } from "@solana/wallet-adapter-react-ui";
 import "@solana/wallet-adapter-react-ui/styles.css";
+import { Airdrop } from "./components/Airdrop";
 
 function App() {
   return (
-    <ConnectionProvider
-      endpoint={"https://solana-devnet.g.alchemy.com/v2/tlv2QFFNvRh8kBN0QY6W4"}
-    >
-      <WalletProvider wallets={[]} autoConnect>
-        <WalletModalProvider>
-          <WalletMultiButton />
-          <WalletDisconnectButton />
-          <div>Main Page again</div>
-        </WalletModalProvider>
-      </WalletProvider>
-    </ConnectionProvider>
+    <div className="relative min-h-screen bg-neutral-950 text-neutral-100">
+      <div className="lg:px-44 md:px-20">
+        <div className="relative min-h-screen sm:border-l sm:border-r border-neutral-800 py-2 px-2 md:py-4 md:px-4 lg:py-8 lg:px-8">
+          <ConnectionProvider
+            endpoint={
+              "https://solana-devnet.g.alchemy.com/v2/tlv2QFFNvRh8kBN0QY6W4"
+            }
+          >
+            <WalletProvider wallets={[]} autoConnect>
+              <WalletModalProvider>
+                <WalletMultiButton />
+                <WalletDisconnectButton />
+                <div>Main Page again</div>
+                <Airdrop />
+              </WalletModalProvider>
+            </WalletProvider>
+          </ConnectionProvider>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- src/components/Airdrop.tsx
+++ src/components/Airdrop.tsx
@@ -1,3 +1,39 @@
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { useState } from "react";
+
 export function Airdrop() {
-  return <div>Airdrop code here</div>;
+  const [airdropAmount, setAirdropAmount] = useState("");
+  const wallet = useWallet();
+  const { connection } = useConnection();
+
+  async function sendAirDrop() {
+    if (!wallet.publicKey) return alert("No wallet detected.");
+
+    try {
+      await connection.requestAirdrop(
+        wallet.publicKey,
+        Number(airdropAmount) * 1000000000
+      );
+    } catch (err) {
+      console.error("Airdrop failed:", err);
+    }
+  }
+
+  return (
+    <div className="flex gap-4">
+      <input
+        type="text"
+        value={airdropAmount}
+        onChange={(e) => setAirdropAmount(e.target.value)}
+        placeholder="Enter Amount"
+        className="border border-neutral-800 px-4 py-1 rounded-sm outline-none"
+      />
+      <button
+        className="px-4 py-1 bg-blue-600 rounded-sm cursor-pointer hover:bg-blue-800 transition-colors duration-200"
+        onClick={sendAirDrop}
+      >
+        Send Airdrop
+      </button>
+    </div>
+  );
 }
